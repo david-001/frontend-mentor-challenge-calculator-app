ba87877676015e0050d730185616a4aed46737a3
David Akim <davidjohnakim@hotmail.com>
2024-02-18 19:46:56 -0400

    test git add

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,6 +3,9 @@ import Screen from "./Screen";
 import NumpadContainer from "./NumpadContainer";
 
 const Calculator = () => {
+  // fsl s;l fsl fslf slfslf slf sfls flsf slfsf sf sfs
+  // dasdads
+  // podxp popfd
   return (
     <div>
       <Screen className="screen-numpad" />
